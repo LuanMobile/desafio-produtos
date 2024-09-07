6843b101da520ba0cd2d7872d745baaaf161e866
LuanMobile <luanhenriique.ls13@gmail.com>
2024-09-07 18:41:15 -0300

    Create moddel, migration, seeder of products

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Cria 50 produtos fictícios
+        Product::factory()->count(50)->create();
+    }
+}
